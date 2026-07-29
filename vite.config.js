97da16d9bde560b54e42f6cfb503f9f8ecb95bc1
shuvokabir360 <shuvokabir360@gmail.com>
2026-07-29 14:15:00 +0600

    Add GitHub Pages automatic build & deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Ensures assets load correctly on GitHub Pages and custom domain
   server: {
     port: 3000,
     open: false
